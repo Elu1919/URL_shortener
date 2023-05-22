0888711c6b07dec7ed474839e43d703c69738a31
Elu1919 <qw29149974@gmail.com>
2023-05-22 14:29:31 +0800

    feat: add Schema

---
+++ models/shorten.js
@@ -0,0 +1,17 @@
+// Require
+const mongoose = require('mongoose')
+
+// Variable Declaration
+const Schema = mongoose.Schema()
+
+const shortenSchema = new Schema({
+  originalLink: {
+    type: String,
+    required: true
+  },
+  shortenLink: {
+    type: String,
+  }
+})
+
+module.exports = mongoose.model('Shorten', shortenSchema)
